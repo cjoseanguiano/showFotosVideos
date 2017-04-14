006544176cc216c13bb774489a9ab7cefbb54a24
Carlos Anguiano <carlos.anguiano@gmail.com>
2017-04-14 00:30:21 -0500

    Optimización de onclick Original con Fragment Final

--- app/src/main/java/com/devix/www/fragmentfotosvideosonClick/MediaListRowHolder.java
+++ app/src/main/java/com/devix/www/fragmentfotosvideosonClick/MediaListRowHolder.java
@@ -4,14 +4,16 @@ package com.devix.www.fragmentfotosvideosonClick;
  *For more info contact : c.joseanguiano@gmail.com
  */
 /*
+ */
+
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.devix.www.fragmentfotosvideos.R;
 
 public class MediaListRowHolder extends RecyclerView.ViewHolder {
-    //    private final View view;
     public View view;
     protected ImageView thumbnail;
     protected TextView title;
@@ -21,66 +23,10 @@ public class MediaListRowHolder extends RecyclerView.ViewHolder {
         this.thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
         this.title = (TextView) view.findViewById(R.id.title);
         this.view = view;
-//        this.view = view;
-//        view.setOnLongClickListener(this);
-//        view.setOnClickListener(this);
+
     }
-/*
-    @Override
-    public void onClick(View v) {
-        Toast.makeText(v.getContext(), "position = " + getPosition(), Toast.LENGTH_SHORT).show();
-
-    }*/
-/*
-    public void dysplay(String text, boolean isSelected) {
-        thumbnail.setImageResource(R.drawable.logo_slogan);
-        dysplay(isSelected);
-    }
-*/
-/*
-    public void dysplay(boolean isSelected) {
-        thumbnail.setBackgroundResource(isSelected ? R.drawable.logo_slogan : R.drawable.logo_slogan);
-    }
-*/
-/*    @Override
-    public boolean onLongClick(View v) {
-//        Toast.makeText(v.getContext(), "position onLongClick= " + getPosition(), Toast.LENGTH_SHORT).show();
-
-        return false;
-    }
-
-    @Override
-    public void onClick(View v) {
-        Toast.makeText(v.getContext(), "position onClick= " + getPosition(), Toast.LENGTH_SHORT).show();
-
-    }*/
-//}
-/*
-
-public class MediaListRowHolder extends RecyclerView.ViewHolder {
-//    private final View view;
-    protected ImageView thumbnail;
-    protected TextView title;
-
-    public MediaListRowHolder(View view) {
-        super(view);
-        this.thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
-        this.title = (TextView) view.findViewById(R.id.title);
-//        this.view = view;
-//        view.setOnLongClickListener(this);
-//        view.setOnClickListener(this);
-    }
-*/
-/*
-    @Override
-    public void onClick(View v) {
-        Toast.makeText(v.getContext(), "position = " + getPosition(), Toast.LENGTH_SHORT).show();
-
-    }*//*
-
 
     public void dysplay(String text, boolean isSelected) {
-//        textView.setText(text);
         thumbnail.setImageResource(R.drawable.logo_slogan);
         dysplay(isSelected);
     }
@@ -88,20 +34,5 @@ public class MediaListRowHolder extends RecyclerView.ViewHolder {
     public void dysplay(boolean isSelected) {
         thumbnail.setBackgroundResource(isSelected ? R.drawable.logo_slogan : R.drawable.logo_slogan);
     }
-
-*/
-/*    @Override
-    public boolean onLongClick(View v) {
-//        Toast.makeText(v.getContext(), "position onLongClick= " + getPosition(), Toast.LENGTH_SHORT).show();
-
-        return false;
-    }
-
-    @Override
-    public void onClick(View v) {
-        Toast.makeText(v.getContext(), "position onClick= " + getPosition(), Toast.LENGTH_SHORT).show();
-
-    }*//*
 
 }
-*/
--- app/src/main/java/com/devix/www/fragmentfotosvideosonClick/MediaRVAdapter.java
+++ app/src/main/java/com/devix/www/fragmentfotosvideosonClick/MediaRVAdapter.java
@@ -12,13 +12,10 @@ import android.media.MediaMetadataRetriever;
 import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.devix.www.fragmentfotosvideos.R;
@@ -27,80 +24,21 @@ import com.squareup.picasso.Picasso;
 import java.io.File;
 import java.util.List;
 
-public class MediaRVAdapter extends RecyclerView.Adapter<MediaRVAdapter.MediaListRowHolder> {
+public class MediaRVAdapter extends RecyclerView.Adapter<MediaListRowHolder> {
 
-    public interface OnItemClickListener {
-        void onItemClicked(int position);
-    }
-
-    public interface OnItemLongClickListener {
-        boolean onItemLongClicked(int position);
-    }
-
-    private Fragment mfragment;
     private List<DataPictures> itemList;
     private Context mContext;
 
-    public class MediaListRowHolder extends RecyclerView.ViewHolder {
-        //    private final View view;
-        public View view;
-        protected ImageView thumbnail;
-        protected TextView title;
-
-        public MediaListRowHolder(View view) {
-            super(view);
-            this.thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
-            this.title = (TextView) view.findViewById(R.id.title);
-            this.view = view;
-//        this.view = view;
-//        view.setOnLongClickListener(this);
-//        view.setOnClickListener(this);
-        }
-/*
-    @Override
-    public void onClick(View v) {
-        Toast.makeText(v.getContext(), "position = " + getPosition(), Toast.LENGTH_SHORT).show();
-
-    }*/
-
-        public void dysplay(String text, boolean isSelected) {
-//        textView.setText(text);
-            thumbnail.setImageResource(R.drawable.logo_slogan);
-            dysplay(isSelected);
-        }
-
-        public void dysplay(boolean isSelected) {
-            thumbnail.setBackgroundResource(isSelected ? R.drawable.logo_slogan : R.drawable.logo_slogan);
-        }
-
-/*    @Override
-    public boolean onLongClick(View v) {
-//        Toast.makeText(v.getContext(), "position onLongClick= " + getPosition(), Toast.LENGTH_SHORT).show();
-
-        return false;
-    }
-
-    @Override
-    public void onClick(View v) {
-        Toast.makeText(v.getContext(), "position onClick= " + getPosition(), Toast.LENGTH_SHORT).show();
-
-    }*/
-    }
 
     public MediaRVAdapter(Context context, List<DataPictures> itemList) {
         this.itemList = itemList;
         this.mContext = context;
     }
 
-    public MediaRVAdapter(Fragment fragment) {
-        mfragment = fragment;
-    }
-
     @Override
     public MediaListRowHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
         View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_row, null);
         MediaListRowHolder mh = new MediaListRowHolder(v);
-
         return mh;
     }
 
@@ -109,7 +47,6 @@ public class MediaRVAdapter extends RecyclerView.Adapter<MediaRVAdapter.MediaLis
         try {
             DataPictures item = itemList.get(i);
 
-//            mediaListRowHolder.title.setText(Html.fromHtml(item.getFileName())); Visualizar nombre archivo
             Uri uri = Uri.fromFile(new File(item.getFilePath()));
             if (item.getFileType().equalsIgnoreCase("video")) {
                 Bitmap bmThumbnail = ThumbnailUtils.
@@ -148,8 +85,7 @@ public class MediaRVAdapter extends RecyclerView.Adapter<MediaRVAdapter.MediaLis
         mediaListRowHolder.view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(mContext, "XD " + i, Toast.LENGTH_SHORT).show();
-//                Toast.makeText(v.getContext(), "position = " + getPosition(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext, "Tomar valor " + i, Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -157,7 +93,6 @@ public class MediaRVAdapter extends RecyclerView.Adapter<MediaRVAdapter.MediaLis
 
     @Override
     public int getItemCount() {
-//        return (null != itemList ? itemList.size() : 0);
-        return itemList.size();
+        return (null != itemList ? itemList.size() : 0);
     }
 }
